a5092bdca60e0b72ff3870463ebc1c10a099f6f8
Jacob <jacob.pancoast17@gmail.com>
2026-02-23 15:59:20 -0500

    Add a square object

--- game_view.py
+++ game_view.py
@@ -15,6 +15,23 @@ class GameView(arcade.Window):
         super().__init__(c.WIDTH, c.HEIGHT, c.TITLE)
 
         self.background_color = c.background
+        self.object_list = None
+
+    def setup(self):
+        self.object_list = arcade.SpriteList(
+            use_spatial_hash=True
+        )
+
+        square = arcade.SpriteSolidColor(
+            width = 100,
+            height = 100,
+            center_x = 600,
+            center_y = 400,
+            color = arcade.csscolor.BLACK,
+            angle = 0
+        )
+
+        self.object_list.append(square) 
 
     '''
     on_draw redraws each frame
@@ -23,7 +40,10 @@ class GameView(arcade.Window):
     return: nothing
     '''
     def on_draw(self):
+        # Reset window
         self.clear()
+
+        self.object_list.draw()
 
     def run_window(self):
         self.setup()
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import game_view as GameView
 
 # Create a GameView and run it
 window = GameView.GameView()
-window.run()
+window.run_window()
