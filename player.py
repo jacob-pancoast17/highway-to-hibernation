fed571955526c5812ed967b4ef7264dff7c7ff0f
mmjaj <m.m.jajkiewicz@gmail.com>
2026-03-20 17:06:15 -0400

    changed player sprite

--- player.py
+++ player.py
@@ -9,7 +9,7 @@ class Player(arcade.Sprite):
     def __init__(self, row, column):
         # For now just makes cubes
         # Right now this also ignores the angle parameter
-        super().__init__(path_or_texture="sprites/bear.png",
+        super().__init__(path_or_texture="sprites/bear_2.png",
             scale = 1.25)
         
         self.center_x = (c.MARGIN + c.TILE_WIDTH) * column + c.MARGIN + c.TILE_WIDTH // 2
