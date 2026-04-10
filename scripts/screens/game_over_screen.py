592778c17bff34938b38b43dbc0d72614bbee6a0
Jacob <jacob.pancoast17@gmail.com>
2026-04-09 20:28:44 -0400

    Resolution affects game over screen and victory screen

--- scripts/screens/game_over_screen.py
+++ scripts/screens/game_over_screen.py
@@ -20,12 +20,25 @@ class GameOver(arcade.View):
         self.score = score
         self.previous_view = previous_view
 
+        self.initialize()
+
+    def initialize(self):
+        '''
+        initialize is part of the game over screen's constructor, but has a special property.
+        initialize only sets logic that is dependent on the size of the screen, which must
+        be updated every time the user changes the resolution in settings.
+
+        param:
+            self
+        returns:
+            nothing
+        '''
         self.game_over_text = arcade.Text(
             "YOU ARE DEAD",
             x = c.WINDOW_WIDTH / 2,
             y = c.WINDOW_HEIGHT * 3 / 4,
             color= c.blood_mwahaha,
-            font_size = 50,
+            font_size = 50 * c.RESOLUTION_RATIO,
             font_name="Edit Undo BRK",
             anchor_x = 'center',
             anchor_y = 'center'
@@ -35,7 +48,7 @@ class GameOver(arcade.View):
             f"SCORE: {self.score}",
             x = c.WINDOW_WIDTH / 2,
             y = c.WINDOW_HEIGHT * 5 / 8,
-            font_size = 30,
+            font_size = 30 * c.RESOLUTION_RATIO,
             font_name="Edit Undo BRK",
             anchor_x = 'center',
             anchor_y = 'center'
@@ -45,7 +58,7 @@ class GameOver(arcade.View):
             "Click to play again",
             x = c.WINDOW_WIDTH / 2,
             y = c.WINDOW_HEIGHT / 2,
-            font_size = 18,
+            font_size = 18 * c.RESOLUTION_RATIO,
             font_name="Edit Undo BRK",
             anchor_x = 'center',
             anchor_y = 'center'
@@ -53,8 +66,8 @@ class GameOver(arcade.View):
         self.quit_text = arcade.Text(
             "Press 'Q' to quit",
             x = c.WINDOW_WIDTH / 2,
-            y = (c.WINDOW_HEIGHT / 2)-30,
-            font_size = 18,
+            y = (c.WINDOW_HEIGHT / 2)-(30 * c.RESOLUTION_RATIO),
+            font_size = 18 * c.RESOLUTION_RATIO,
             font_name="Edit Undo BRK",
             anchor_x = 'center',
             anchor_y = 'center'
@@ -62,8 +75,8 @@ class GameOver(arcade.View):
         self.stats_text = arcade.Text(
             "Press 'S' for stats",
             x=c.WINDOW_WIDTH / 2,
-            y=(c.WINDOW_HEIGHT / 2) - 60,
-            font_size = 18,
+            y=(c.WINDOW_HEIGHT / 2) - (60 * c.RESOLUTION_RATIO),
+            font_size = 18 * c.RESOLUTION_RATIO,
             font_name="Edit Undo BRK",
             anchor_x = 'center',
             anchor_y = 'center'
